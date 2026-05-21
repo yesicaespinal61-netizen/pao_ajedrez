7f32e8867ec6d0bc32e56f7944f0d4bf6c882603
yesicaespinal61-netizen <yesicaespinal61@gmail.com>
2026-05-21 09:38:09 -0500

    Inicio de mi juego
    empezar a jugar

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("Hola mundo, voy a crear mi juego")
+import pygame
+import sys
+
+pygame.init()
+
+DIMENSION = 8  
+TAMANO_CASILLA = 80 
+ANCHO = ALTO = DIMENSION * TAMANO_CASILLA
+
+COLOR_BLANCO = (255, 255, 255)
+COLOR_NEGRO = (0, 0, 0)
+
+
+pantalla = pygame.display.set_mode((ANCHO, ALTO))
+pygame.display.set_caption("Tablero Blanco y Negro")
+
+ejecutando = True
+while ejecutando:
+    
+   
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            ejecutando = False
+
+    for fila in range(DIMENSION):
+        for columna in range(DIMENSION):
+
+            if (fila + columna) % 2 == 0:
+                color = COLOR_BLANCO
+            else:
+                color = COLOR_NEGRO
+            
+           
+            pygame.draw.rect(pantalla, color, pygame.Rect(columna * TAMANO_CASILLA, fila * TAMANO_CASILLA, TAMANO_CASILLA, TAMANO_CASILLA))
+
+ 
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
